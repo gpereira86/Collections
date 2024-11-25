6e499c1db22f303188d67957006aee5e5243d70a
Glauco Pereira <glaucoadm@gmail.com>
2024-11-25 11:02:05 -0300

    Collections Initial Commit

---
+++ curso-dio-intro-collections/src/br/com/dio/collection/list/ExemploList.java
@@ -0,0 +1,24 @@
+package br.com.dio.collection.list;
+
+public class ExemploList {
+
+    public static void main(String[] args) {
+        //Dada uma lista com 7 nota de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
+
+        //  List notas = new ArrayList(); //antes do java 5
+        //  List<Double> notas = new ArrayList(); //Generics(JDK 5) - Diamont Operator (JDK 7)
+        //  ArrayList<Double> notas = new ArrayList();
+        //  List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        /*  List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+            notas.add(10d)
+            System.out.println(notas);
+         */
+        /*  List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+            notas.add(1d);
+            notas.remove(5d);
+            System.out.println(notas);
+         */
+
+    }
+
+}
